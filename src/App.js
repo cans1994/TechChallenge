060d8becaae8f544e30faa758f1010068f470c5e
cans1994 <cans.bl@hotmail.com>
2022-09-19 00:45:43 +0200

    ajustements

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App() {
   return (
     <div className="App">
       <header>
-      <title>Les Argonautes</title>
             <h1>
               <img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
               Les Argonautes
